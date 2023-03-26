e659380f3521cd0cd7da145c174d39cf03afb180
ddmkim94 <ddmkim94@gmail.com>
2023-03-26 23:45:00 +0900

    #24 - 길 안내 url 구현
    지도 바로가기 url, 로드뷰 바로가기 url 구현

--- src/main/java/com/fastcampus/pharmacy/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/fastcampus/pharmacy/pharmacy/service/PharmacyRecommendationService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +24,9 @@ public class PharmacyRecommendationService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
     public List<OutputDto> recommendPharmacyList(String address) {
 
@@ -50,11 +54,26 @@ public class PharmacyRecommendationService {
     }
 
     private OutputDto convertToOutputDto(Direction direction) {
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()),
+                String.valueOf(direction.getTargetLongitude()));
+
+        /**
+         * 한글 이름 인코딩
+         * KakaoUriBuilderService 와 다르게 String 으로 바로 받는 이유는
+         * RestTemplate 를 사용하지 않기 때문이다.
+         * url 을 String 으로 줄 수 있지만 RestTemplate 내부에서도 인코딩을 진행하기 때문에 문제가 발생할 수도 있음
+          */
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
+
+        log.info("direction params: {} url: {}", params, result);
+
         return OutputDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
-                .directionUrl("todo") // todo: 추가 예정!
-                .roadViewUrl("todo")
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
                 .build();
     }
